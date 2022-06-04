3b7830187b4b68c9dfa47a8c23e417e3c02ea52a
Dawid Dudek <addme96@gmail.com>
2022-06-04 19:58:08 +0200

    extracted resource fetching to a separate component

--- simple-frontend/src/App.js
+++ simple-frontend/src/App.js
@@ -1,33 +1,11 @@
 import React from "react";
 import logo from './logo.svg';
 import './App.css';
-import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import './components/GridResources'
+import GridResources from "./components/GridResources";
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            resources: [],
-            loaded: false
-        }
-    }
-
-    render() {
-        const {loaded, resources} = this.state;
-        if (!loaded){
-            return (
-                <div>
-                    <h1> Loading resources.... </h1>
-                </div>
-            );
-        }
-
-
-        const columns: GridColDef[] = [
-            { field: 'id', headerName: 'ID', width: 100 },
-            { field: 'name', headerName: 'Resource Name', width: 300 },
-        ];
-
+      render() {
         return (
             <div className="App">
                 <header className="App-header">
@@ -42,16 +20,7 @@ class App extends React.Component {
                     <p>
                         The below data is fetched from Golang simple backend service which in turn fetches data from PostgreSQL DB.
                     </p>
-                    <div style={{ height: 400, width: '100%', backgroundColor: "whitesmoke" }}>
-                      <DataGrid
-                          rows={resources}
-                          columns={columns}
-                          pageSize={5}
-                          rowsPerPageOptions={[5]}
-                          checkboxSelection
-                      />
-                  </div>
-
+                    <GridResources />
                 <a
                     className="App-link"
                     href="https://reactjs.org"
@@ -64,16 +33,6 @@ class App extends React.Component {
             </div>
         );
   }
-
-    componentDidMount() {
-        setTimeout("", 100000);
-        fetch(process.env.REACT_APP_BACKEND_DNS_NAME + "/resources")
-            .then(response => response.json())
-            .then(data => this.setState({
-                resources: data,
-                loaded: true
-            }));
-    }
 }
 
 
---
+++ simple-frontend/src/components/GridResources.js
@@ -0,0 +1,50 @@
+import React from "react";
+import {DataGrid, GridColDef} from "@mui/x-data-grid";
+
+class GridResources extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            resources: [],
+            loaded: false
+        }
+    }
+    render() {
+        const {loaded, resources} = this.state;
+        if (!loaded){
+            return (
+                <div>
+                    <h1> Loading resources.... </h1>
+                </div>
+            );
+        }
+
+        const columns: GridColDef[] = [
+            { field: 'id', headerName: 'ID', width: 100 },
+            { field: 'name', headerName: 'Resource Name', width: 300 },
+        ];
+        return (
+            <div style={{ height: 400, width: '100%', backgroundColor: "whitesmoke" }}>
+                <DataGrid
+                    rows={resources}
+                    columns={columns}
+                    pageSize={5}
+                    rowsPerPageOptions={[5]}
+                    checkboxSelection
+                />
+            </div>
+        );
+    }
+
+    componentDidMount() {
+        setTimeout("", 100000);
+        fetch(process.env.REACT_APP_BACKEND_DNS_NAME + "/resources")
+            .then(response => response.json())
+            .then(data => this.setState({
+                resources: data,
+                loaded: true
+            }));
+    }
+}
+
+export default GridResources;
