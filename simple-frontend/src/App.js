2fa6a1654dfddf19f817ab2480bae983b8528eb5
Dawid Dudek <addme96@gmail.com>
2022-06-16 21:46:20 +0200

    introduced layout

--- simple-frontend/src/App.js
+++ simple-frontend/src/App.js
@@ -1,53 +1,77 @@
 import React from "react";
 import logo from './logo.svg';
 import './App.css';
-import {NavLink, Outlet} from "react-router-dom"
-class App extends React.Component {
-    render() {
+import {NavLink, Outlet, Route, Routes} from "react-router-dom"
+import Resources from "./routes/resources";
+import Resource from "./routes/resource";
+import About from "./routes/about";
+export default function App() {
         return (
             <div className="App">
                 <header className="App-header">
-                    <nav>
-                        <ol>
-                            <li>
-                                <NavLink to="/" style={({ isActive }) => {
-                                    return {
-                                        display: "block",
-                                        margin: "1rem 0",
-                                        color: isActive ? "red" : "white",
-                                    };
-                                }}>Home</NavLink>
-                            </li>
-                            <li>
-                                <NavLink to="/resources" style={({ isActive }) => {
-                                    return {
-                                        display: "block",
-                                        margin: "1rem 0",
-                                        color: isActive ? "red" : "white",
-                                    };
-                                }}>Resources</NavLink>
-                            </li>
-                            <li>
-                                <NavLink to="/about" style={({ isActive }) => {
-                                    return {
-                                        display: "block",
-                                        margin: "1rem 0",
-                                        color: isActive ? "red" : "white",
-                                    };
-                                }}>About</NavLink>
-                            </li>
-                        </ol>
-                    </nav>
                     <img src={logo} className="App-logo" alt="logo"/>
                     <p>
                         Hello!
                     </p>
-                    <Outlet/>
                 </header>
+                <Routes>
+                    <Route path="/" element={<Layout />}>
+                        <Route path="resources" element={<Resources/>}>
+                            <Route path=":resourceID" element={<Resource/>}/>
+                        </Route>
+                        <Route path="about" element={<About/>}/>
+                        <Route
+                            path="*"
+                            element={
+                                <main style={{padding: "1rem"}}>
+                                    <p>There's nothing here!</p>
+                                </main>
+                            }
+                        />
+                    </Route>
+                </Routes>
             </div>
         );
-    }
 }
 
+function Layout() {
+    return (
+        <div>
+            <nav>
+                <ul>
+                    <li>
+                        <NavLink to="/" style={({ isActive }) => {
+                            return {
+                                display: "block",
+                                margin: "1rem 0",
+                                color: isActive ? "red" : "black",
+                            };
+                        }}>Home</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/resources" style={({ isActive }) => {
+                            return {
+                                display: "block",
+                                margin: "1rem 0",
+                                color: isActive ? "red" : "black",
+                            };
+                        }}>Resources</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/about" style={({ isActive }) => {
+                            return {
+                                display: "block",
+                                margin: "1rem 0",
+                                color: isActive ? "red" : "black",
+                            };
+                        }}>About</NavLink>
+                    </li>
+                </ul>
+            </nav>
 
-export default App;
+            <hr />
+
+            <Outlet />
+        </div>
+    );
+}
--- simple-frontend/src/index.js
+++ simple-frontend/src/index.js
@@ -2,31 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {BrowserRouter, Routes, Route} from "react-router-dom";
-import Resources from "./routes/resources"
-import Resource from "./routes/resource";
-import About from "./routes/about"
+import {BrowserRouter} from "react-router-dom";
+
 ReactDOM.render(
-    <BrowserRouter>
-        <React.StrictMode>
-            <Routes>
-                <Route path="/" element={<App />} >
-                    <Route path="resources" element={<Resources />} >
-                        <Route path=":resourceID" element={<Resource />} />
-                    </Route>
-                    <Route path="about" element={<About />} />
-                    <Route
-                        path="*"
-                        element={
-                            <main style={{ padding: "1rem" }}>
-                                <p>There's nothing here!</p>
-                            </main>
-                        }
-                    />
-                </Route>
-            </Routes>
-        </React.StrictMode>
-    </BrowserRouter>
+    <React.StrictMode>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+    </React.StrictMode>
     , document.getElementById('root')
 );
 
