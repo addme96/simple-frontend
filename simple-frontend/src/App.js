122416f88ea2d944855cfd3724c6ccb2b05b7507
Dawid Dudek <addme96@gmail.com>
2022-06-19 17:12:51 +0200

    switched to react-admin

--- simple-frontend/src/App.js
+++ simple-frontend/src/App.js
@@ -1,88 +1,53 @@
 import React from "react";
 import './App.css';
-import {NavLink, Outlet, Route, Routes, useLocation} from "react-router-dom"
-import Resources from "./routes/resources";
-import Resource from "./routes/resource";
-import About from "./routes/about";
-import Modal from "./components/modal";
+import {
+    Admin,
+    Resource as AdminResource,
+    Datagrid,
+    List,
+    TextField,
+    EditButton,
+    Edit,
+    Create,
+    SimpleForm,
+    TextInput,
+} from 'react-admin';
+import dataProvider from "./dataProvider";
+
 export default function App() {
-    let location = useLocation();
-
-    // The `backgroundLocation` state is the location that we were at when one of
-    // the resources links was clicked. If it's there, use it as the location for
-    // the <Routes> so we show the resources grid in the background, behind the modal.
-    let state =  { backgroundLocation: location.state};
-        return (
-            <div className="App">
-                {/*<header className="App-header">*/}
-                {/*    <img src={logo} className="App-logo" alt="logo"/>*/}
-                {/*    <p>*/}
-                {/*        Hello!*/}
-                {/*    </p>*/}
-                {/*</header>*/}
-                <Routes location={state?.backgroundLocation || location}>
-                    <Route path="/" element={<Layout />}>
-                        <Route path="resources" element={<Resources/>}>
-                            <Route path=":resourceID" element={<Resource/>}/>
-                        </Route>
-                        <Route path="about" element={<About/>}/>
-                        <Route path="*" element={
-                                <main style={{padding: "1rem"}}>
-                                    <p>There's nothing here!</p>
-                                </main>
-                            }
-                        />
-                    </Route>
-                </Routes>
-
-                {/* Show the modal when a `backgroundLocation` is set */}
-                {state?.backgroundLocation && (
-                    <Routes>
-                        <Route path="/resources/:resourceID" element={<Modal />} />
-                    </Routes>
-                )}
-            </div>
-        );
-}
-
-function Layout() {
     return (
-        <div>
-            <nav>
-                <ul>
-                    <li>
-                        <NavLink to="/" style={({ isActive }) => {
-                            return {
-                                display: "block",
-                                margin: "1rem 0",
-                                color: isActive ? "red" : "black",
-                            };
-                        }}>Home</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/resources" style={({ isActive }) => {
-                            return {
-                                display: "block",
-                                margin: "1rem 0",
-                                color: isActive ? "red" : "black",
-                            };
-                        }}>Resources</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/about" style={({ isActive }) => {
-                            return {
-                                display: "block",
-                                margin: "1rem 0",
-                                color: isActive ? "red" : "black",
-                            };
-                        }}>About</NavLink>
-                    </li>
-                </ul>
-            </nav>
-
-            <hr />
-
-            <Outlet />
+        <div className="App">
+            <Admin dataProvider={dataProvider}>
+                <AdminResource name="resources" list={ResourcesList} edit={ResourceEdit} create={ResourceCreate}/>
+            </Admin>
         </div>
     );
 }
+
+
+const ResourcesList = () => (
+    <List>
+        <Datagrid bulkActionButtons={false}>
+            <TextField source="id"/>
+            <TextField source="name"/>
+            <EditButton />
+        </Datagrid>
+    </List>
+);
+
+const ResourceEdit = () => (
+    <Edit>
+        <SimpleForm>
+            <TextInput source="id" />
+            <TextInput source="name" />
+        </SimpleForm>
+    </Edit>
+)
+
+const ResourceCreate = () => (
+    <Create>
+        <SimpleForm>
+            <TextInput source="name" />
+        </SimpleForm>
+    </Create>
+)
---
+++ simple-frontend/src/dataProvider.js
@@ -0,0 +1,40 @@
+import { fetchUtils } from 'react-admin';
+
+const apiUrl =  process.env.REACT_APP_BACKEND_DNS_NAME;
+const httpClient = fetchUtils.fetchJson;
+
+const dataProvider = {
+    getList: (resource, params) => {
+        const url = `${apiUrl}/${resource}`;
+        return httpClient(url).then(({ headers, json }) => ({
+            data: json,
+            total: json.length,
+        }));
+    },
+
+    getOne: (resource, params) =>
+        httpClient(`${apiUrl}/${resource}/${params.id}`).then(({ json }) => ({
+            data: json,
+        })),
+
+    update: (resource, params) =>
+        httpClient(`${apiUrl}/${resource}/${params.id}`, {
+            method: 'PUT',
+            body: JSON.stringify(params.data),
+        }).then(({ json }) => ({ data: json })),
+
+    create: (resource, params) =>
+        httpClient(`${apiUrl}/${resource}`, {
+            method: 'POST',
+            body: JSON.stringify(params.data),
+        }).then(({ json }) => ({
+            data: { ...params.data, id: json.id },
+        })),
+
+    delete: (resource, params) =>
+        httpClient(`${apiUrl}/${resource}/${params.id}`, {
+            method: 'DELETE',
+        }).then(({ json }) => ({ data: json })),
+};
+
+export default dataProvider;
